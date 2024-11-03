fe3fcbc5c036a48c83a056c1b9978ec464fdc2ec
Vitalii Mikhnevych <blackrooty@gmail.com>
2024-11-03 20:22:26 +0100

    add heathcheck site

--- rootfs/bin/check.sh
+++ rootfs/bin/check.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-ret=$(curl  -s http://ip-api.com/json | jq '.countryCode' | tr '[:upper:]' '[:lower:]' | tr -d '"'|  grep "$COUNTRY" | wc -l)
+#ret=$(curl  -s http://ip-api.com/json | jq '.countryCode' | tr '[:upper:]' '[:lower:]' | tr -d '"'|  grep "$COUNTRY" | wc -l)
+ret=$(curl  -s https://ifconfig.io/country_code | tr '[:upper:]' '[:lower:]' | tr -d '"'|  grep "$COUNTRY" | wc -l)
 if [ $ret -eq 1 ]; then
     echo 0
 else
