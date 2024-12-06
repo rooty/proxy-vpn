61299d2a8cf545e77d4d24c526023da0fe03a2b5
Vitalii Mikhnevych <blackrooty@gmail.com>
2024-12-06 20:48:08 +0100

    add proxy auth #fix 2

--- Dockerfile
+++ Dockerfile
@@ -53,5 +53,5 @@ ENTRYPOINT ["/bin/docker-entrypoint.sh"]
 
 # Configure a healthcheck to validate that everything is up&running
 #HEALTHCHECK --timeout=10s CMD curl --silent --fail http://127.0.0.1:8080/fpm-ping || exit 1
-#HEALTHCHECK  CMD /bin/check.sh
+HEALTHCHECK  CMD /bin/check.sh
 
--- rootfs/bin/check.sh
+++ rootfs/bin/check.sh
@@ -2,8 +2,8 @@
 #ret=$(curl  -s http://ip-api.com/json | jq '.countryCode' | tr '[:upper:]' '[:lower:]' | tr -d '"'|  grep "$COUNTRY" | wc -l)
 ret=$(curl  -s --proxy localhost:8888  https://ifconfig.io/country_code | tr '[:upper:]' '[:lower:]' | tr -d '"'|  grep "$COUNTRY" | wc -l)
 if [ $ret -eq 1 ]; then
-    echo 0
+    exit 0
 else
-    echo 1
+    exit 1
 fi
 
