1993f481040b76ccb36f9116a889db4fd04120d5
Vitalii Mikhnevych <blackrooty@gmail.com>
2025-02-15 20:50:02 +0100

    fix HEALTHCHEK_URL env

--- rootfs/bin/check.sh
+++ rootfs/bin/check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ret=$(curl  -s http://ip-api.com/json | jq '.countryCode' | tr '[:upper:]' '[:lower:]' | tr -d '"'|  grep "$COUNTRY" | wc -l)
-#ret=$(curl  --proxy-anyauth --proxy-user "$PROXY_USER:$PROXY_PASS" -s --proxy localhost:8888  https://ifconfig.io/country_code | tr '[:upper:]' '[:lower:]' | tr -d '"'|  grep "$COUNTRY" | wc -l)
+#ret=$(curl  --proxy-anyauth --proxy-user "$PROXY_USER:$PROXY_PASS" -s --proxy localhost:8888  ${HEALTHCHEK_URL:-https://ifconfig.io/country_code} | tr '[:upper:]' '[:lower:]' | tr -d '"'|  grep "$COUNTRY" | wc -l)
 if [ $ret -eq 1 ]; then
     exit 0
 else
